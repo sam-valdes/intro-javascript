f4475233e07676044af98511b5b99a6cb16934bc
sam-valdes <samvaldesr@gmail.com>
2022-03-10 12:31:45 -0600

    Create actividad1.js

---
+++ actividad1.js
@@ -0,0 +1,23 @@
+//actividad 1.1- Mostrar el número mayor
+var num1=Number( prompt("Ingresa un número"))
+
+var result=num1+2
+console.log(result)
+
+
+//actividad 1.2- Resta de dos números
+var num1=Number( prompt("Ingresa un número"))
+var num2=Number( prompt("Ingresa otro número"))
+var result=num1-num2
+console.log("La resta de ", num1, " y ", num2, " es ",result)
+
+//actividad 1.3- Resta de dos números
+r=5.3
+var result=Math.PI*(r**2)
+console.log("El área de un círculo con ", r, " es ", result)
+
+//actividad 1.4- Resta de dos números
+var d=6
+var D=8
+var result=(D*d)/2
+console.log("El área de un rombo es ", result)
