73f18987cb3eae13f87446943a692e8081a26427
sam-valdes <samvaldesr@gmail.com>
2022-03-31 11:36:43 -0600

    proyecto final

--- ProyectoFinal/app2.js
+++ ProyectoFinal/app2.js
@@ -46,34 +46,57 @@ const Accounts = [
 
 
 //f(x) sam
-        // function login(form){
-        //     if (Users.includes(form.u.value)) {
-        //         if (form.n.value=Accounts[Users.indexOf(form.u.value)].password) {
-        //             location="principal.html";
-        //         } else {
-        //             alert("NIP Incorrecto");
-        //         }
-        //     } else {
-        //         alert("Usuario no encontrado. Favor de comunicarse al 5533445500");
-        //         location="index2.html";
-        //     }
-        // }
-                
+let usuario='sam'
+let saldo=1
 
-
-        // f(x) gabri
         function login(form){
-            for(var i = 0 ; i < Accounts.length; i++){
-              if(form.u.value = Accounts[i].name){
-                if(form.n.value = Accounts[i].password){
-                  location = "principal.html";
-                } else{
-                  alert("NIP Incorrecto");
+            if (Users.includes(form.u.value)) {
+                if (form.n.value==Accounts[Users.indexOf(form.u.value)].password) {
+                    location="main.html";
+                    usuario=form.u.value;
+                    saldo=Accounts[Users.indexOf(form.u.value)].balance;
+                    alert('Bienvenido',usuario,'tu saldo es ',saldo)
+
+                } else {
+                    alert("NIP Incorrecto");
                 }
-              } 
+            } else {
+                alert("Usuario no encontrado. Favor de comunicarse al 5533445500");
+                
+                
             }
-            if(location != "principal.html"){
-              alert("Usuario no encontrado. Favor de comunicarse al 5533445500");
-              location="index2.html";
-            }
-          }
+        }
+                
+        function login_go(){
+          location="login.html";
+          
+        }
+
+        function withdraw_go(){
+          location="withdraw.html"
+        }
+        function deposit_go(){
+          location="deposit.html"
+        }
+
+        function inicio_go(){
+          location="inicio.html"
+        }
+        function balance_go(){
+          location="balance.html"
+        }
+        
+        function main_go(){
+          location="main.html"
+        }
+
+
+        
+
+        let objetivo = document.getElementById('saldoprint');
+        objetivo.innerHTML = Number(saldo); 
+        let objetivo2 = document.getElementById('usuarioprint');
+        objetivo2.innerHTML = Number(usuario); 
+
+        // document.getElementById("usuarioprint").innerHTML = usuario;
+        // document.getElementById("saldoprint").innerHTML = saldo;
